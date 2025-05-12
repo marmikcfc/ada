ce1fe18de83c30f826188121d3e448342b91520c
Marmik Pandya <marmikpandya@Marmiks-MacBook-Pro.local>
2025-05-12 16:58:20 +0530

    add end to end voice infrastructue

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import VoiceBotClient from './components/VoiceBotClient';
 
