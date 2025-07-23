2501e83e2729d96ece96299db0a91429ef49efb5
Marmik Pandya <marmikpandya@Marmiks-MacBook-Pro.local>
2025-07-23 10:05:07 +0530

    remove genux sdk

--- packages/genux-sdk/src/components/composite/index.ts
+++
@@ -1,5 +0,0 @@
-// Composite component exports
-export { ChatWindow, type ChatWindowProps } from './ChatWindow';
-export { MinimizableChatWindow, type MinimizableChatWindowProps } from './MinimizableChatWindow';
-export { ThreadedChatWindow, type ThreadedChatWindowProps } from './ThreadedChatWindow';
-export { FullscreenLayout, type FullscreenLayoutProps } from './FullscreenLayout';
